b6835863abe40a7b2674d02a57163378bbce723c
anton <anton.mi.ju@gmail.com>
2019-07-18 21:07:23 +0300

    lesson2 added http exception pages

--- src/main/java/com/findme/controller/UserController.java
+++ src/main/java/com/findme/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.findme.controller;
 
-import com.findme.exceptions.BadRequestException;
 import com.findme.exceptions.NotFoundException;
+import com.findme.exceptions.SystemException;
 import com.findme.models.User;
 import com.findme.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,14 +21,18 @@ public class UserController {
     }
 
     @RequestMapping(path = "/user/{userId}", method = RequestMethod.GET)
-    public String profile(Model model, @PathVariable String userId) throws Exception{
+    public String profile(Model model, @PathVariable String userId) {
         try {
             User user = userService.get(Long.parseLong(userId));
             if (user == null)
                 throw new NotFoundException("404: User is not found.");
             model.addAttribute("user", userService.get(Long.parseLong(userId)));
         } catch (NumberFormatException e){
-            throw new BadRequestException("400: You write incorrect id.");
+            return "badRequestException";
+        } catch (NotFoundException e){
+            return "notFoundException";
+        } catch (SystemException e){
+            return "systemException";
         }
         return "profile";
     }
